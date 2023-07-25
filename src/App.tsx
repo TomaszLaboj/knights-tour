0eaae032bbf58b26b8007302ad2756c3e592b03c
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-25 20:22:45 +0100

    added knight after click

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./app.css";
 
 const rows: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
 const columns: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
-// const knight: string = "♘"
+const knight = "♞"
 
 //-------------------------------------------------------------
 
@@ -13,41 +13,47 @@ function App(): JSX.Element {
   const SquaresData: SquareElement[] = [];
   createArray(SquaresData);
   const [boardState,setBoardState] = useState<SquareElement[]>(SquaresData)
+
   //------------------------------------------------------------- 
   interface SquareProps{
-    square: SquareElement;
+    square: SquareElement; 
   }
   //-------------------------------------------------------------
   const OneSquare = (props:SquareProps) => {
     return(
-      <div className={(props.square.row+props.square.column)%2===0?"white":"black"} 
-      key="{props.square.id}">
+      <div className={(props.square.row+props.square.column)%2===0?"white":"black"}
+      onClick={()=>handleMovePiece(props.square)} 
+      key={props.square.id}>
         
-      
+       
         <div>{props.square.piece}</div>
         <div>{props.square.moveCounter===0?'':props.square.moveCounter}</div>
       </div>
     )
   }
   //-------------------------------------------------------------
-  const board:JSX.Element[]= [];
+  const handleMovePiece= (clickedSquare:SquareElement) => {
+    
 
+      const indexOfClickedSquare:number = boardState.indexOf(clickedSquare);
+      const tempArray:SquareElement[] = boardState;
+      tempArray[indexOfClickedSquare].piece = knight;
+      setBoardState([...tempArray]);
+      console.log(boardState)
+      
+      
+  }
+  const board:JSX.Element[]= [];
+  
   for(const element of boardState){
     board.push(<OneSquare square={element}/>)
   }
+  return (
+  <div className="board">
 
-  
-  
-  
-  
-  
-  
-  
-
-  return (<div className="board">
     {board}
    
-    </div>)
+  </div>)
 }
 
 export default App;
