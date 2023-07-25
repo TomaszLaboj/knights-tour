c67618c00f0569e9a7123a33bd9cd8fc7a141fcb
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-25 21:31:52 +0100

    clickable horse

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,53 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import "./app.css";
 
 const rows: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
 const columns: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
-const knight = "♞"
+const knight = "♞";
 
 //-------------------------------------------------------------
 
 function App(): JSX.Element {
-
   const SquaresData: SquareElement[] = [];
   createArray(SquaresData);
-  const [boardState,setBoardState] = useState<SquareElement[]>(SquaresData)
+  const [boardState, setBoardState] = useState<SquareElement[]>(SquaresData);
 
-  //------------------------------------------------------------- 
-  interface SquareProps{
-    square: SquareElement; 
+  //-------------------------------------------------------------
+  interface SquareProps {
+    square: SquareElement;
   }
   //-------------------------------------------------------------
-  const OneSquare = (props:SquareProps) => {
-    return(
-      <div className={(props.square.row+props.square.column)%2===0?"white":"black"}
-      onClick={()=>handleMovePiece(props.square)} 
-      key={props.square.id}>
-        
-       
+  const OneSquare = (props: SquareProps) => {
+    return (
+      <div
+        className={
+          (props.square.row + props.square.column) % 2 === 0 ? "white" : "black"
+        }
+        onClick={() => handleMovePiece(props.square)}
+        key={props.square.id}
+      >
         <div>{props.square.piece}</div>
-        <div>{props.square.moveCounter===0?'':props.square.moveCounter}</div>
+        <div>
+          {props.square.moveCounter === 0 ? "" : props.square.moveCounter}
+        </div>
       </div>
-    )
-  }
+    );
+  };
   //-------------------------------------------------------------
-  const handleMovePiece= (clickedSquare:SquareElement) => {
-    
+  const handleMovePiece = (clickedSquare: SquareElement) => {
+    const indexOfClickedSquare: number = boardState.indexOf(clickedSquare);
+    const tempArray: SquareElement[] = boardState;
+    tempArray[indexOfClickedSquare].piece = knight;
+    setBoardState([...tempArray]);
+    console.log(boardState);
+  };
+  const board: JSX.Element[] = [];
 
-      const indexOfClickedSquare:number = boardState.indexOf(clickedSquare);
-      const tempArray:SquareElement[] = boardState;
-      tempArray[indexOfClickedSquare].piece = knight;
-      setBoardState([...tempArray]);
-      console.log(boardState)
-      
-      
+  for (const element of boardState) {
+    board.push(<OneSquare square={element} />);
   }
-  const board:JSX.Element[]= [];
-  
-  for(const element of boardState){
-    board.push(<OneSquare square={element}/>)
-  }
-  return (
-  <div className="board">
-
-    {board}
-   
-  </div>)
+  return <div className="board">{board}</div>;
 }
 
 export default App;
